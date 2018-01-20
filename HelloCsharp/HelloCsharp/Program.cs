4103c2744f4996cba6701677dc08466564aa6ebe
HyundongHwang <hhd2002@gmail.com>
2018-01-20 23:19:25 +0900

    modify

---
+++ HelloCsharp/HelloCsharp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello c#"); //DT000000
+            Console.ReadLine();
+        }
+    }
+}
