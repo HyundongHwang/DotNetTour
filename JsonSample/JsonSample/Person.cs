f1a7a5ca11d022cc5ea2dca6f0391c84123f82f2
HyundongHwang <hhd2002@gmail.com>
2018-01-22 17:05:39 +0900

    modify

---
+++ JsonSample/JsonSample/Person.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonSample
+{
+    // DT001010
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Natianality { get; set; }
+        public int BirthYear { get; set; }
+
+        // DT001011
+        [JsonIgnore]
+        public int Age
+        {
+            get
+            {
+                var age = DateTime.Now.Year - this.BirthYear;
+                return age;
+            }
+        }
+    }
+}
---
+++ JsonSample/JsonSample/Program.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // DT001020
+            var people = new List<Person>
+            {
+                new Person
+                {
+                    Name = "이순신",
+                    Natianality = "조선",
+                    BirthYear = 1598,
+                },
+
+                new Person
+                {
+                    Name = "김유신",
+                    Natianality = "신라",
+                    BirthYear = 595,
+                },
+
+                new Person
+                {
+                    Name = "왕건",
+                    Natianality = "고려",
+                    BirthYear = 877,
+                },
+            };
+
+            // DT001021
+            var jsonStr = JsonConvert.SerializeObject(people, Formatting.Indented);
+            Console.WriteLine($"jsonStr : {jsonStr}");
+
+            // DT001022
+            var people2 = JsonConvert.DeserializeObject<List<Person>>(jsonStr);
+            Console.WriteLine($"people2.Count : {people2.Count}");
+            Console.WriteLine($"people2[0].Name : {people2[0].Name}");
+            Console.WriteLine($"people2[0].Age : {people2[0].Age}");
+
+            Console.ReadLine();
+        }
+    }
+}
